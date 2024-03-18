554ff32a49fea542c94cbee6577e7ce214f251b3
Carlos Enrique <44654304+carlosepcc@users.noreply.github.com>
2024-03-18 01:38:43 -0400

    fix: responsive cards

--- components/App/Logo.vue
+++ components/App/Logo.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 .flex.items-center.gap-2
     q-avatar(square)
-        img(src="isotype.svg" )
+        img(src="/isotype.svg" )
     h1.tracking-wide.font-bold.text-3xl.italic.font-serif Perversionis
     p.font-light.tracking-wider.uppercase Pecados Peculiares
 </template>
